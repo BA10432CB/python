b7a140b10f173ac0d16b68b9a3246fefda6b26f5
BA10432CB <prest.0808.bass@gmail.com>
2023-08-05 12:19:54 +0900

    vending_machine

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,9 @@
-for i in range(1, 101):
+for i in range(1, 101): #1 <= i < 101 => 1 <= i <=100
     if i % 15 == 0:
-        print('FizzBuzz')
+        print('FizzBuzz', end=' ') #end=' 'を要素に付け加えると改行せずに空白を出力する
     elif i % 3 == 0:
-        print('Fizz')
+        print('Fizz', end=' ')
     elif i % 5 == 0:
-        print('Buzz')
+        print('Buzz', end=' ')
     else:
-        print(i)
-
-#例
-#for i in range(1, 101):
-#   if (i % 3 == 0) and (i % 5 == 0):
-#       print('FizzBuzz')
-#   elif i % 3 == 0:
-#       print('Fizz')
-#   elif i % 5 == 0:
-#       print('Buzz')
-#   else:
-#       print(i)
+        print(i, end=' ')
